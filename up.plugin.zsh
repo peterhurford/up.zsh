ae8bb5ff9a7d44095f78023ce675c0b3747d45ee
Peter Hurford <peter@peterhurford.com>
2015-10-12 12:02:38 -0500

    Fix control flow and error on bad usage. Resolves #1

--- up.plugin.zsh
+++ up.plugin.zsh
@@ -3,15 +3,18 @@
 # https://github.com/peterhurford/up.zsh
 
 up(){                                 # Go up X directories (default 1)
-  local d=""
-  limit=$1
-  for ((i=1 ; i <= limit ; i++))
-    do
-      d=$d/..
-    done
-  d=$(echo $d | sed 's/^\///')
-  if [ -z "$d" ]; then
-    d=..
+  if [[ "$#" -ne 1 ]]; then
+    cd ..
+  elif ! [[ $1 =~ '^[0-9]+$' ]]; then
+    echo "Error: up should be called with the number of directories to go up. The default is 1."
+  else
+    local d=""
+    limit=$1
+    for ((i=1 ; i <= limit ; i++))
+      do
+        d=$d/..
+      done
+    d=$(echo $d | sed 's/^\///')
+    cd $d
   fi
-  cd $d
 }
